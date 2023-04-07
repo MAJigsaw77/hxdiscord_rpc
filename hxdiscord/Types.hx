c45c24d5954910f61aa75fae233c68fe3f73921f
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2023-04-07 23:39:38 +0300

    Update Types.hx

--- hxdiscord/Types.hx
+++ hxdiscord/Types.hx
@@ -16,21 +16,21 @@ extern class DiscordRichPresence
 	@:native('DiscordRichPresence')
 	static function create():DiscordRichPresence;
 
-	var state:String; /* max 128 bytes */
-	var details:String; /* max 128 bytes */
+	var state:cpp.ConstCharStar; /* max 128 bytes */
+	var details:cpp.ConstCharStar; /* max 128 bytes */
 	var startTimestamp:cpp.Int64;
 	var endTimestamp:cpp.Int64;
-	var largeImageKey:String; /* max 32 bytes */
-	var largeImageText:String; /* max 128 bytes */
-	var smallImageKey:String; /* max 32 bytes */
-	var smallImageText:String; /* max 128 bytes */
-	var partyId:String; /* max 128 bytes */
+	var largeImageKey:cpp.ConstCharStar; /* max 32 bytes */
+	var largeImageText:cpp.ConstCharStar; /* max 128 bytes */
+	var smallImageKey:cpp.ConstCharStar; /* max 32 bytes */
+	var smallImageText:cpp.ConstCharStar; /* max 128 bytes */
+	var partyId:cpp.ConstCharStar; /* max 128 bytes */
 	var partySize:Int;
 	var partyMax:Int;
 	var partyPrivacy:Int;
-	var matchSecret:String; /* max 128 bytes */
-	var joinSecret:String; /* max 128 bytes */
-	var spectateSecret:String; /* max 128 bytes */
+	var matchSecret:cpp.ConstCharStar; /* max 128 bytes */
+	var joinSecret:cpp.ConstCharStar; /* max 128 bytes */
+	var spectateSecret:cpp.ConstCharStar; /* max 128 bytes */
 	var instance:cpp.Int8;
 }
 
@@ -44,10 +44,10 @@ extern class DiscordUser
 	@:native('DiscordUser')
 	static function create():DiscordUser;
 
-	var userId:String;
-	var username:String;
-	var discriminator:String;
-	var avatar:String;
+	var userId:cpp.ConstCharStar;
+	var username:cpp.ConstCharStar;
+	var discriminator:cpp.ConstCharStar;
+	var avatar:cpp.ConstCharStar;
 }
 
 @:buildXml("<include name='${haxelib:hxdiscord-rpc}/project/Build.xml' />")
@@ -61,9 +61,9 @@ extern class DiscordEventHandlers
 	static function create():DiscordEventHandlers;
 
 	var ready:cpp.Callable<(request:cpp.RawConstPointer<DiscordUser>) -> Void>;
-	var disconnected:cpp.Callable<(errorCode:Int, message:String) -> Void>;
-	var errored:cpp.Callable<(errorCode:Int, message:String) -> Void>;
-	var joinGame:cpp.Callable<(joinSecret:String) -> Void>;
-	var spectateGame:cpp.Callable<(spectateSecret:String) -> Void>;
+	var disconnected:cpp.Callable<(errorCode:Int, message:cpp.ConstCharStar) -> Void>;
+	var errored:cpp.Callable<(errorCode:Int, message:cpp.ConstCharStar) -> Void>;
+	var joinGame:cpp.Callable<(joinSecret:cpp.ConstCharStar) -> Void>;
+	var spectateGame:cpp.Callable<(spectateSecret:cpp.ConstCharStar) -> Void>;
 	var joinRequest:cpp.Callable<(request:cpp.RawConstPointer<DiscordUser>) -> Void>;
 }
