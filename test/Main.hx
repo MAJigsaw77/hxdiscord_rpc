913533d7e287c042fc0d9604c5a9f850e77527dd
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2023-04-08 13:01:19 +0300

    Update Main.hx

--- test/Main.hx
+++ test/Main.hx
@@ -11,10 +11,6 @@ class Main
 		handlers.ready = cpp.Function.fromStaticFunction(onReady);
 		handlers.disconnected = cpp.Function.fromStaticFunction(onDisconnected);
 		handlers.errored = cpp.Function.fromStaticFunction(onError);
-		handlers.joinGame = cpp.Function.fromStaticFunction(onJoin);
-		handlers.spectateGame = cpp.Function.fromStaticFunction(onSpectate);
-		handlers.joinRequest = cpp.Function.fromStaticFunction(onJoinRequest);
-
 		Discord.Initialize("345229890980937739", cpp.RawPointer.addressOf(handlers), 1, null);
 
 		while (true)
@@ -30,8 +26,8 @@ class Main
 
 	static function onReady(request:cpp.RawConstPointer<DiscordUser>):Void
 	{
-		var requestRef:cpp.Star<DiscordUser> = cast request;
-		Sys.println('Discord: connected to user ' + requestRef.username + '#' + requestRef.discriminator + '\n' + requestRef.userId);
+		var requestPtr:cpp.Star<DiscordUser> = cpp.ConstPointer.fromRaw(request).ptr;
+		Sys.println('Discord: connected to user ' + requestPtr.username + '#' + requestPtr.discriminator + ' ' + requestPtr.userId);
 	}
 
 	static function onDisconnected(errorCode:Int, message:cpp.ConstCharStar):Void
@@ -39,17 +35,4 @@ class Main
 
 	static function onError(errorCode:Int, message:cpp.ConstCharStar):Void
 		Sys.println('Discord: error (' + errorCode + ': ' + message + ')');
-
-	static function onJoin(joinSecret:cpp.ConstCharStar):Void
-		Sys.println('Discord: join (' + joinSecret + ')');
-
-	static function onSpectate(spectateSecret:cpp.ConstCharStar):Void
-		Sys.println('Discord: spectate (' + spectateSecret + ')');
-
-	static function onJoinRequest(request:cpp.RawConstPointer<DiscordUser>):Void
-	{
-		var requestRef:cpp.Star<DiscordUser> = cast request;
-		Sys.println('Discord: join request from ' + requestRef.username + '#' + requestRef.discriminator + '\n' + requestRef.userId);
-		Discord.Respond(requestRef.userId, Discord.REPLY_NO);
-	}
 }
