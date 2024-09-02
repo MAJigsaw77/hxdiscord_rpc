1eba8c8a5b19c92bff15dca485b321a7543c7571
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-09-02 08:49:32 +0300

    Add buttons for the sample.

--- sample/Main.hx
+++ sample/Main.hx
@@ -8,7 +8,7 @@ class Main
 {
 	public static function main():Void
 	{
-		var handlers:DiscordEventHandlers = DiscordEventHandlers.create();
+		final handlers:DiscordEventHandlers = DiscordEventHandlers.create();
 		handlers.ready = cpp.Function.fromStaticFunction(onReady);
 		handlers.disconnected = cpp.Function.fromStaticFunction(onDisconnected);
 		handlers.errored = cpp.Function.fromStaticFunction(onError);
@@ -36,26 +36,41 @@ class Main
 
 	private static function onReady(request:cpp.RawConstPointer<DiscordUser>):Void
 	{
-		if (Std.parseInt(cast(request[0].discriminator, String)) != 0)
-			Sys.println('Discord: Connected to user (${cast(request[0].username, String)}#${cast(request[0].discriminator, String)})');
-		else
-			Sys.println('Discord: Connected to user (${cast(request[0].username, String)})');
+		final username:String = request[0].username;
+		final globalName:String = request[0].username;
+		final discriminator:String = request[0].discriminator;
 
-		var discordPresence:DiscordRichPresence = DiscordRichPresence.create();
+		if (Std.parseInt(cast(request[0].discriminator, String)) != 0)
+			Sys.println('Discord: Connected to user ${username}#${discriminator} ($globalName)');
+		else
+			Sys.println('Discord: Connected to user @${username} ($globalName)');
+
+		final discordPresence:DiscordRichPresence = DiscordRichPresence.create();
 		discordPresence.state = "West of House";
 		discordPresence.details = "Frustration";
 		discordPresence.largeImageKey = "canary-large";
 		discordPresence.smallImageKey = "ptb-small";
+
+		final button:DiscordButton = DiscordButton.create();
+		button.label = "Test 1";
+		button.url = "https://example.com";
+		discordPresence.buttons[0] = button;
+
+		final button:DiscordButton = DiscordButton.create();
+		button.label = "Test 2";
+		button.url = "https://discord.gg/fortnite";
+		discordPresence.buttons[1] = button;
+
 		Discord.UpdatePresence(cpp.RawConstPointer.addressOf(discordPresence));
 	}
 
 	private static function onDisconnected(errorCode:Int, message:cpp.ConstCharStar):Void
 	{
-		Sys.println('Discord: Disconnected ($errorCode: ${cast(message, String)})');
+		Sys.println('Discord: Disconnected ($errorCode:$message)');
 	}
 
 	private static function onError(errorCode:Int, message:cpp.ConstCharStar):Void
 	{
-		Sys.println('Discord: Error ($errorCode: ${cast(message, String)})');
+		Sys.println('Discord: Error ($errorCode:$message)');
 	}
 }
