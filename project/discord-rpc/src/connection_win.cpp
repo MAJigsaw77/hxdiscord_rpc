d1cc2fe5cdaa08aa629da57ad0894b445cfa77ed
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-09-11 10:22:42 +0300

    Update connection_win.cpp

--- project/discord-rpc/src/connection_win.cpp
+++ project/discord-rpc/src/connection_win.cpp
@@ -4,7 +4,7 @@
 
 int GetProcessId()
 {
-	return (int)::GetCurrentProcessId();
+	return (int) GetCurrentProcessId();
 }
 
 struct BaseConnectionWin : public BaseConnection
@@ -31,11 +31,13 @@ bool BaseConnection::Open()
 	wchar_t pipeName[]{L"\\\\?\\pipe\\discord-ipc-0"};
 	const size_t pipeDigit = sizeof(pipeName) / sizeof(wchar_t) - 2;
 	pipeName[pipeDigit] = L'0';
+
 	auto self = reinterpret_cast<BaseConnectionWin *>(this);
+
 	for (;;)
 	{
-		self->pipe =
-		    ::CreateFileW(pipeName, GENERIC_READ | GENERIC_WRITE, 0, nullptr, OPEN_EXISTING, 0, nullptr);
+		self->pipe = CreateFileW(pipeName, GENERIC_READ | GENERIC_WRITE, 0, nullptr, OPEN_EXISTING, 0, nullptr);
+
 		if (self->pipe != INVALID_HANDLE_VALUE)
 		{
 			self->isOpen = true;
@@ -43,6 +45,7 @@ bool BaseConnection::Open()
 		}
 
 		auto lastError = GetLastError();
+
 		if (lastError == ERROR_FILE_NOT_FOUND)
 		{
 			if (pipeName[pipeDigit] < L'9')
@@ -54,9 +57,8 @@ bool BaseConnection::Open()
 		else if (lastError == ERROR_PIPE_BUSY)
 		{
 			if (!WaitNamedPipeW(pipeName, 10000))
-			{
 				return false;
-			}
+
 			continue;
 		}
 
@@ -67,72 +69,54 @@ bool BaseConnection::Open()
 bool BaseConnection::Close()
 {
 	auto self = reinterpret_cast<BaseConnectionWin *>(this);
-	::CloseHandle(self->pipe);
+
+	CloseHandle(self->pipe);
+
 	self->pipe = INVALID_HANDLE_VALUE;
 	self->isOpen = false;
+
 	return true;
 }
 
 bool BaseConnection::Write(const void *data, size_t length)
 {
 	if (length == 0)
-	{
 		return true;
-	}
-
-	if (!data)
-	{
-		return false;
-	}
 
 	auto self = reinterpret_cast<BaseConnectionWin *>(this);
 
 	if (!self || self->pipe == INVALID_HANDLE_VALUE)
-	{
 		return false;
-	}
 
 	const DWORD bytesLength = (DWORD)length;
 	DWORD bytesWritten = 0;
-	return ::WriteFile(self->pipe, data, bytesLength, &bytesWritten, nullptr) == TRUE &&
-	       bytesWritten == bytesLength;
+	return WriteFile(self->pipe, data, bytesLength, &bytesWritten, nullptr) == TRUE && bytesWritten == bytesLength;
 }
 
 bool BaseConnection::Read(void *data, size_t length)
 {
-	if (!data)
-	{
-		return false;
-	}
-
 	auto self = reinterpret_cast<BaseConnectionWin *>(this);
 
 	if (!self || self->pipe == INVALID_HANDLE_VALUE)
-	{
 		return false;
-	}
 
 	DWORD bytesAvailable = 0;
-	if (::PeekNamedPipe(self->pipe, nullptr, 0, nullptr, &bytesAvailable, nullptr))
+
+	if (PeekNamedPipe(self->pipe, nullptr, 0, nullptr, &bytesAvailable, nullptr))
 	{
 		if (bytesAvailable >= length)
 		{
 			DWORD bytesToRead = (DWORD)length;
 			DWORD bytesRead = 0;
-			if (::ReadFile(self->pipe, data, bytesToRead, &bytesRead, nullptr) == TRUE)
-			{
+
+			if (ReadFile(self->pipe, data, bytesToRead, &bytesRead, nullptr) == TRUE)
 				return true;
-			}
 			else
-			{
 				Close();
-			}
 		}
 	}
 	else
-	{
 		Close();
-	}
 
 	return false;
 }
